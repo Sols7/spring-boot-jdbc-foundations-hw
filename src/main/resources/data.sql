ef218f9bf8f9161a067bc2244c59437f8c5e2a66
Sols7 <serg-lex@yandex.ru>
2022-02-26 23:34:56 +0300

    add project spring-boot-jdbc-foundations-hw

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO users(id, name, age) VALUES (1, 'u1', 18);
+INSERT INTO users(id, name, age) VALUES (2, 'u2', 22);
+INSERT INTO users(id, name, age) VALUES (3, 'u3', 20);
+INSERT INTO users(id, name, age) VALUES (4, 'u4', 11);
